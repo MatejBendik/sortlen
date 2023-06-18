c0e4433c791f9f77dd801aff0f920bdee9bfc45d
MatejBendik <matejbendik.mb@gmail.com>
2023-06-18 21:14:25 +0200

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer class="fixed bottom-0 bg-slate-100 w-full">
+      <div class="mx-auto max-w-screen-xl p-4 text-center">
+        <span class="text-sm text-gray-900">
+          Made with ❤️ by{" "}
+          <a
+            href="https://twitter.com/BendikMatej"
+            class="text-blue-500 hover:text-blue-600"
+            target="_blank"
+          >
+            Matej Bendík
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
